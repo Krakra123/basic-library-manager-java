903b390e9c3edcb0e9de2c0bc857b35318526487
Krakra123 <nghiabn2005@gmail.com>
2024-10-12 17:14:36 +0700

    Basic UI of MainPane + Update JavaFX to 23, Java to 22

---
+++ src/main/java/app/MainPaneController.java
@@ -0,0 +1,26 @@
+package app;
+
+import javafx.fxml.FXML;
+
+public class MainPaneController {
+    
+    @FXML
+    private void handleMenuButton() {
+
+    }
+
+    @FXML
+    private void handleLibraryButton() {
+        
+    }
+
+    @FXML
+    private void handleSettingButton() {
+        
+    }
+
+    @FXML
+    private void handleInfoButton() {
+        
+    }
+}
