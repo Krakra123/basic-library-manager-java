937c55140a5ce135440df53cc5ba346deaf69964
Krakra123 <nghiabn2005@gmail.com>
2024-10-16 15:56:41 +0700

    Revert "Add testing scene"
    This reverts commit 21ae20a740e908440e7be3a021c930daa3289b0c.

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,31 +1,32 @@
 package app;
 
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
-    private static Scene scene;
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("basic-window"));
+    public void start(Stage stage) throws Exception {
+        Label label = new Label("Hello World");
+        label.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(label, 500, 350);
+
+        stage.setTitle("Application");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    public static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    @Override
+    public void stop() throws Exception {
+        super.stop();
     }
 
     public static void main(String[] args) {
--- src/main/java/app/Tab2.java
+++
@@ -1,9 +0,0 @@
-package app;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-
-public class Tab2 {
-
-    
-}
