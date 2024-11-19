cf85179a2066e1312f9f228c4f6dcb18b8dba189
longnbh <hoanglongjimmy2005@gmail.com>
2024-11-20 01:23:05 +0700

    change design of User Library UI

--- src/main/java/app/data/Library.java
+++ src/main/java/app/data/Library.java
@@ -5,25 +5,25 @@ import java.util.List;
 
 public class Library {
 
-	private BookCollection listOfBooks = new BookCollection();
-	private List<Member> listOfMembers = new ArrayList<Member>();
-	public Library() {
-		
-	}
-	
-	public void clearLibrary() {
-        listOfBooks.clear();
-    }
-	
-	public void addBook(Book book) {
-        listOfBooks.add(book);
-    }
-	
-	public void removeBookById(String id) {
-		listOfBooks.removeBookById(id);
-	}
-	
-	public Book findBookById(String id) {
-		return listOfBooks.findBookById(id);
-	}
+//	private BookCollection listOfBooks = new BookCollection();
+//	private List<Member> listOfMembers = new ArrayList<Member>();
+//	public Library() {
+//
+//	}
+//
+//	public void clearLibrary() {
+//        listOfBooks.clear();
+//    }
+//
+//	public void addBook(Book book) {
+//        listOfBooks.add(book);
+//    }
+//
+//	public void removeBookById(String id) {
+//		listOfBooks.removeBookById(id);
+//	}
+//
+//	public Book findBookById(String id) {
+//		return listOfBooks.findBookById(id);
+//	}
 }
