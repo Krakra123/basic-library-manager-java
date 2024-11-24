6bd0fd87afbd21cfcf047db5088be1838c252f1c
Krakra123 <nghiabn2005@gmail.com>
2024-11-24 21:22:12 +0700

    Add more dialog

--- src/main/java/app/managers/DialogsManager.java
+++ src/main/java/app/managers/DialogsManager.java
@@ -1,14 +1,23 @@
 package app.managers;
 
+import app.controller.ConfirmDialogController;
 import app.controller.SavingBookDialogController;
 import app.interfaces.ICallback;
 
 public class DialogsManager extends BaseManager {
     
+    private static final String CONFIRM_DIALOG_FXML = "ConfirmDialog";
     private static final String SAVING_BOOK_DIALOG_FXML = "SavingBookDialog";
 
     public DialogsManager(AppManager manager) {
         super(manager);
+    }
+
+    public void showConfirmDialog(ICallback confirmCallback, String title, String detail) {
+        DialogHandler dialog = new DialogHandler(manager, CONFIRM_DIALOG_FXML);
+        dialog.show(confirmCallback);
+        dialog.fxmlData.getController(ConfirmDialogController.class).setManager(dialog);
+        dialog.fxmlData.getController(ConfirmDialogController.class).setText(title, detail);
     }
 
     public void showSavingBookDialog(ICallback savingBookCallback) {
