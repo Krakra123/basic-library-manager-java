549267980a7b6b4a298ea69cce67410392fbc904
Krakra123 <nghiabn2005@gmail.com>
2024-11-25 18:43:45 +0700

    minor fixes

--- src/main/java/app/managers/MainDisplayManager.java
+++ src/main/java/app/managers/MainDisplayManager.java
@@ -9,8 +9,8 @@ public class MainDisplayManager extends BaseManager {
     
     private static final String MAIN_DISPLAY_FXML = "MainDisplay";
     private static final String TOOLBAR_FXML = "Toolbar";
-    private static final int WINDOW_WIDTH = 1200;
-    private static final int WINDOW_HEIGHT = 750;
+    private static final int WINDOW_WIDTH = 1150;
+    private static final int WINDOW_HEIGHT = 700;
 
     private LoadableFXMLContent mainDisplayFXMLContent;
     public LoadableFXMLContent getMainDisplayFXMLContent() {
